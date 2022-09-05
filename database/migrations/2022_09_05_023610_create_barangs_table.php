db67141c57abcc692d8a26edbaaa9af17bbb6bfa
Veri Iswanto <dev.veriiswanto@gmail.com>
2022-09-05 09:54:44 +0700

    manambahkan migration

---
+++ database/migrations/2022_09_05_023610_create_barangs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('barang', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->integer('harga');
+            $table->integer('stok');
+            $table->integer('suplier_id');
+            $table->integer('kategori_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('barang');
+    }
+};
